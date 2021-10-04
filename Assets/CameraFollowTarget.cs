cd0349735ce064ce39f5c51969c3ee4ebcc76784
OnionKiller <tomi@csilling.com>
2021-10-04 16:37:04 +0200

    camera follows player

---
+++ Assets/CameraFollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public GameObject target;
+    public float followspeed = 2.0f;
+    void Update()
+    {
+        float dt = Time.deltaTime;
+        float fspeed = Mathf.Min(1.0f, followspeed * dt);
+        Vector3 oldPos = transform.position;
+        Vector3 newPos = oldPos;
+        newPos.x = target.transform.position.x;
+        newPos = oldPos + (newPos - oldPos) * fspeed;
+        transform.position = newPos;
+    }
+}
